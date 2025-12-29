1bb40ab42fbe1ae59a4c25e7d28684ff69e621ab
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2025-12-29 17:57:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,33 @@
-LOCAL_PATH := device/xiaomi/blossom
+#
+# Copyright (C) 2023 The OrangeFox Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/realme/RMX2185
 
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Dynamic Partitions
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
+    fastbootd
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Bypass anti-rollback ROMs protection
+# Set build date to Jan 1 2009 00:00:00
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.date.utc=1230768000
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
